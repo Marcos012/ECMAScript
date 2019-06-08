7ca87f46d4eaba851cc125168634aa1c0dc700a5
marcos012 <marcospaulo.ad@gmail.com>
2019-06-08 15:02:04 -0300

    Exemplo de metodos string

---
+++ 03 - melhorias/metodos-string.js
@@ -0,0 +1,6 @@
+let text = 'Lorem ipsum dolor sit amet';
+
+console.log(text.startsWith('rem', 2));
+console.log(text.endsWith('ame', 25));
+console.log(text.includes('ipsum'));
+console.log('lol'.repeat(10));
