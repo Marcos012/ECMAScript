7c31c816e142e740992e7ed0be0f7d8dcd93aa05
marcos012 <marcospaulo.ad@gmail.com>
2019-06-08 20:11:06 -0300

    Exemplo com padend

---
+++ es8/padstart-padend.js
@@ -0,0 +1,9 @@
+console.log("Marcos".padStart(10));
+console.log("Marcos".padStart(11, "Olá: "));
+console.log("Marcos".padStart(13, "654321 "));
+console.log("Marcos".padStart(10, "0 "));
+
+console.log("Marcos".padEnd(16, " bem-vindo"));
+console.log("Marcos".padEnd(13, " 654321"));
+console.log("Marcos".padEnd(10, " 0"));
+console.log("Marcos".padEnd(1));           
--- es8/padstart.js
+++
@@ -1,5 +0,0 @@
-console.log("Marcos".padStart(10));
-console.log("Marcos".padStart(11, "Olá: "));
-console.log("Marcos".padStart(13, "654321 "));
-console.log("Marcos".padStart(10, "0 "));
-console.log("Marcos".padStart(1));           
