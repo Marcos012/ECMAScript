5db74a2727cada313773e4aaeac4fb65008a5cf4
marcos012 <marcospaulo.ad@gmail.com>
2019-06-08 20:07:19 -0300

    Exemplo com padstart

---
+++ es8/padstart.js
@@ -0,0 +1,5 @@
+console.log("Marcos".padStart(10));
+console.log("Marcos".padStart(11, "Olá: "));
+console.log("Marcos".padStart(13, "654321 "));
+console.log("Marcos".padStart(10, "0 "));
+console.log("Marcos".padStart(1));           
